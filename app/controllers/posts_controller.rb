8b848c86bd5dc1ee1356bfaaf56ad193946f21ad
ragapinilih <ragapinilih@gmail.com>
2014-03-17 23:47:27 +0700

    Ruby on Rails Coding Conventions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,51 @@
 class PostsController < ApplicationController
-	
-	http_basic_authenticate_with name: "1207", password: "1207", except: [:index, :show]
+  
+  http_basic_authenticate_with name: "1207", password: "1207", except: [:index, :show]
 
-	def index
-		@posts = Post.all.order('id DESC')	
-	end
+  def index
+    @posts = Post.all.order('id DESC')  
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-		@post = Post.new(post_params)
-		@post.save
-		if @post.save
-			redirect_to @post
-		else
-			render 'new'
-		end
-	end
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 
-	def show
-		@post = Post.find(params[:id])	
-	end
+  def show
+    @post = Post.find(params[:id])  
+  end
 
-	def edit
-		@post = Post.find(params[:id])
-	end
+  def edit
+    @post = Post.find(params[:id])
+  end
 
-	def update
-		@post = Post.find(params[:id])
+  def update
+    @post = Post.find(params[:id])
 
-		if @post.update(params[:post].permit(:title, :text))
-			redirect_to @post
-		else
-			render 'edit'
-		end
-	end
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@post = Post.find(params[:id])
-		@post.destroy
-		redirect_to posts_path
-	end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
-	private
-		def post_params
-			params.require(:post).permit(:title, :text)
-		end
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  validates :commenter, presence: true, length: { minimum: 2 }	
+  validates :commenter, presence: true, length: { minimum: 2 }  
   validates :body, presence: true
 end
